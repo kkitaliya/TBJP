55254b6384d0eaa1afc3b89200adf0e0e1e8fb58
kkitaliya <kriyalitaliya44@gmail.com>
2025-11-15 12:54:53 +0530

    Module add.

---
+++ TBJP/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'TBJP',
+    'version': '1.0',
+    'summary': 'TBJP Customizations',
+    'author': 'TBJP',
+    'depends': ['base', 'contacts','account'],
+    'data': [
+        'views/menu_items.xml',
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False
+}
